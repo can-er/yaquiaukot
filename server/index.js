ea651dae967af9a1f833a539f2296aba4f0b5329
Caner <2broglie@gmail.com>
2020-12-15 22:44:02 +0100

    add of macs & names

--- server/index.js
+++ server/index.js
@@ -1,43 +1,49 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
+//const readline = require('readline');
+
 var macs = []; // array of mac's of devices
 var htmlcontent = "";
+var arr = { "e0:cc:f8:54:4c:3e" : "Spartak", "18:5e:0f:a2:3f:28" : "IliesPC", "8c:f5:a3:df:c8:cc": "Nadir", "6e:de:7c:ee:16:9f":"Caner"};
+
 const server = http.createServer((req, res) => {
     if (req.method === 'POST') {
         // Handle post info...
-	let body = '';
-	req.on('data', chunk => {
+        let body = '';
+        req.on('data', chunk => {
             body += decodeURIComponent(chunk.toString()); // convert Buffer to string
-    	});
+        });
 
-    	req.on('end', () => {
+        req.on('end', () => {
             console.log(body);
-	    fs.writeFile('devices.txt', body, function (err) {
-  	    if (err) return console.log(err);
-	    //console.log('Hello World > helloworld.txt');
-	    });
+            fs.writeFile('devices.txt', body, function (err) {
+            if (err) return console.log(err);
+            //console.log('Hello World > helloworld.txt');
+            });
             res.end('ok');
-    	});
+        });
     }
     else {
       fs.readFile('devices.txt', 'utf8', function(err, contents) {
-	macs = contents.split('=')[1].split(';');
-	//console.log(macs);
+        macs = contents.split('=')[1].split(';');
       });
-      console.log("***");
       console.log(macs);
-      console.log("***");
       htmlcontent = "" ;
       for (var i = 0; i < macs.length; i++) {
-  	htmlcontent += macs[i] + "<br>";
+        var mac_i = macs[i];
+        for (var key in arr){
+                if (mac_i == key){ 
+                        htmlcontent += arr[key] + "<br>";
+                }
+        }
       }
 
       res.end(`
         <!doctype html>
         <html>
         <body>
-		<h1> Y a qui au kot ? </h1>
-		<p> ${htmlcontent} </p>
+                <h1> Y a qui au kot ? </h1>
+                <p> ${htmlcontent} </p>
             <!--form action="/" method="post">
                 <input type="text" name="fname" /><br />
                 <input type="number" name="age" /><br />
